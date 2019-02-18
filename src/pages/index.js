b9ccd7bd3409bb0c2e7eb225088cb21db7bab416
Martin Kikkas <martinkikkas12@gmail.com>
2019-02-18 23:37:37 +0200

    Basic CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learn to design and code React apps</h1>
+      <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+      <Link to="/page-2/">Watch video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Watch video</Link>
   </Layout>
 )
 
