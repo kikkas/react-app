510342346362c4696478e8626900252d332875e8
Martin Kikkas <martinkikkas12@gmail.com>
2019-02-24 01:26:58 +0200

    Footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: `Design+Code 3`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin',
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-contentful',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        spaceId: 'c4bevqigl52d',
+        accessToken: '07d6965992d169631f5332399d3f05c831ca05e0cbe330d7303e590f9c321cfc'
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -12,15 +13,37 @@ const Layout = ({ children }) => (
         site {
           siteMetadata {
             title
+            description
+            keywords
+          }
+        }
+        allContentfulLink (sort: {fields:[createdAt], order: ASC}){
+          edges {
+            node {
+              title
+              url
+              createdAt
+            }
           }
         }
       }
     `}
     render={data => (
       <>
+        <Header 
+        title={data.site.siteMetadata.title}
+        meta={[
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
+        ]} 
+        />
         <div>
-          <Header/>
-          <main>{children}</main>
+          <main>
+          {children}
+          <Footer data={data}>
+            Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
+          </Footer>
+          </main>
         </div>
       </>
     )}
