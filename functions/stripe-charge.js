7e3f6d629bb729cfc5c70b17e5fadfad4b74182f
Martin Kikkas <martinkikkas12@gmail.com>
2019-02-25 23:27:12 +0200

    Stripe payments

--- functions/stripe-charge.js
+++ functions/stripe-charge.js
@@ -1,3 +1,24 @@
-exports.handler = function () {
-    console.log('working...')
+const stripe = require('stripe')('sk_test_q3YnnIptXD5cg1B6DnDXo3lo')
+
+exports.handler = async function (event) {
+    const {
+        tokenId,
+        email,
+        name,
+        description,
+        amount
+    } = JSON.parse(event.body)
+
+    const customer = await stripe.customers.create({
+        description: email,
+        source: tokenId
+    })
+
+    await stripe.charges.create({
+        customer: customer.id,
+        amount,
+        name,
+        description,
+        currency: 'usd'
+    })
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 import logo from '../images/logo-designcode.svg'
 import './Header.css'
+import StripeCheckout from 'react-stripe-checkout'
+
 
 class Header extends React.Component {
   constructor(props) {
@@ -27,6 +29,26 @@ class Header extends React.Component {
       this.setState ({ hasScrolled: false })
     }
   }
+
+
+  handlePurchase = (token) => {
+    const amount = 5000
+    const description = "My awesome product"
+  
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+  
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
   render () {
     return (
       <div>
@@ -36,7 +58,14 @@ class Header extends React.Component {
             <Link to="/courses" >Courses</Link>
             <Link to="/downloads" >Download</Link>
             <Link to="/workshops" >Workshops</Link>
-            <Link to="/buy" ><button>Buy</button></Link>
+            <StripeCheckout
+              amount={5000}
+              image="https://cl.ly/0K2f1V3K3h0D/download/Logo.jpg" 
+              token={this.handlePurchase}
+              stripeKey={'pk_test_QC1WbT3olMS9i4nnE8G0BLrD'}
+              >
+              <button>Buy</button>
+              </StripeCheckout>
             </div>
           </div>
         </div>    
